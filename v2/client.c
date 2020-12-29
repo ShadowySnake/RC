f28604f363e3aa8e3e43c5c8c53654a4b391cd45
ShadowySnake <75637465+ShadowySnake@users.noreply.github.com>
2020-12-29 19:09:15 +0200

    Add files via upload

---
+++ v2/client.c
@@ -0,0 +1,94 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <string.h>
+
+extern int errno;
+
+#define PORT 1031
+
+int main(int argc,char *argv[])
+{
+   int sd; // descriptorul socketului
+   struct sockaddr_in server; 
+   char command[100];
+   char received[100];
+   
+   if((sd = socket(AF_INET,SOCK_STREAM,0)) == -1)
+   {
+     perror("Erroare la deschiderea socketului in client.\n");
+     return errno;
+   }
+   
+   server.sin_family = AF_INET;
+   server.sin_addr.s_addr = inet_addr("127.0.0.1");
+   server.sin_port = htons(PORT);
+   
+   if(connect(sd,(struct sockaddr *) &server, sizeof(struct sockaddr))==-1)
+   {
+    perror("Erroare la connect in client.\n");
+    return errno;
+   }
+   
+   while(1)
+   {
+    printf("Dati comanda: ");
+    fflush(stdout);
+   
+   scanf("%s",&command);
+   fflush(stdin);
+   
+    write(sd,&command,sizeof(command)); // scrierea catre server
+    read(sd,&received,sizeof(received)); // citirea din server
+    
+    printf("The command received from server is: ");
+    printf("%s\n",received);
+    fflush(stdout);
+    
+    if(strcmp(received,"quit")==0) {
+    printf("Ending the connection...\n"); 
+    break; 
+    }
+    else if(strcmp(received,"login")==0){
+    
+          char name[]="adriano";
+          write(sd,&name,sizeof(name));
+          read(sd,&received,sizeof(received));
+          printf("%s\n",received);
+          if(strcmp(received,"SUCCESS") == 0) printf("Successfull login demo!\n");
+          else printf("Unsuccessfull login demo!\n");
+          fflush(stdout);
+         }
+         else if(strcmp(received,"register")==0){
+         
+                read(sd,&received,sizeof(received));
+                if(strcmp(received,"SUCCESS") == 0) printf("Successfull register!\n");
+                else printf("Could not register, username is already taken!\n");
+                fflush(stdout);
+                }
+                else if(strcmp(received,"show_all")==0){
+                
+                      printf("The commands list is as follows:\n");
+                      printf("Unrestricted: 'login' 'register' 'quit'\n");
+                      printf("Commands for all users: 'all_commands' 'show_champs'\n");
+                      printf("Commands for normal users only: 'join_champ' 'postpone'\n");
+                      printf("Commands for administrators only: 'history' 'create_champ'\n");
+                      fflush(stdout);
+                      }
+                      else if( (strcmp(received,"active_champ")==0) || (strcmp(received,"join_champ")==0) || (strcmp(received,"create_champ")==0)|| (strcmp(received,"postpone")==0) || (strcmp(received,"round_history")==0))
+                      { 
+                        printf("NOT YET IMPLEMENTED!\n"); fflush(stdout);
+                      }
+                      else if(strcmp(received,"No commands")==0){
+                       printf("No commands found. Try typing 'all_commands' ( or 'quit' in order to stop ).\n");
+                       }
+   }
+   
+   close(sd);
+   return 0;
+}
